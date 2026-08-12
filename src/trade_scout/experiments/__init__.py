ed9bcf85604b23b80b960c0c32b8c3e63c9bd3b5
Costieman <65705627+Costieman@users.noreply.github.com>
2026-08-13 00:29:55 +0800

    Expose first research program planning API

--- src/trade_scout/experiments/__init__.py
+++ src/trade_scout/experiments/__init__.py
@@ -10,11 +10,20 @@ from trade_scout.experiments.contracts import (
     ResearchStage,
     StageResult,
 )
+from trade_scout.experiments.first_research_program import (
+    FIRST_RESEARCH_PROGRAM,
+    FirstProgramExperiment,
+    FirstProgramGrid,
+    ProgramStep,
+    first_program_step,
+    validate_first_research_program,
+)
 from trade_scout.experiments.runner import ExperimentRunner
 from trade_scout.experiments.store import FileManifestStore
 from trade_scout.experiments.sweeps import expand_grid
 
 __all__ = [
+    "FIRST_RESEARCH_PROGRAM",
     "ExperimentContext",
     "ExperimentDefinition",
     "ExperimentExecutionError",
@@ -22,8 +31,13 @@ __all__ = [
     "ExperimentRunner",
     "ExperimentStatus",
     "FileManifestStore",
+    "FirstProgramExperiment",
+    "FirstProgramGrid",
+    "ProgramStep",
     "ResearchMode",
     "ResearchStage",
     "StageResult",
     "expand_grid",
+    "first_program_step",
+    "validate_first_research_program",
 ]
